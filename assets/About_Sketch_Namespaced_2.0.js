c3684ac24054e115d4ad52d8c0f51ae7dd83874f
Kit Nicholson <kitnicholson@protonmail.com>
2026-09-17 22:37:21 +1000

    editing about sketch

--- assets/About_Sketch_Namespaced_2.0.js
+++ assets/About_Sketch_Namespaced_2.0.js
@@ -7,7 +7,7 @@ var sketch = function(p) {
 
   p.imgScale;
 
-  p.randomness = 250; // how far the features can move
+  p.randomness = 555; // how far the features can move
 
   p.frameChange = 2;
 
@@ -34,16 +34,20 @@ var sketch = function(p) {
   }
 
   p.setup = function() {
-    p.canvas = p.createCanvas(p.windowWidth-1, p.windowHeight);
+
+    p.canvas = p.createCanvas(p.windowWidth, p.windowHeight);
     p.canvas.position(0,0);
     p.imageMode(CENTER);
-    p.frameRate(1.2);
+    p.frameRate(1.4);
     // p.frameRate(30);
     p.count = 0;
 
     p.getCenterPos();
 
-    p.imgScale = p.height * 0.0006;   
+    p.euclidDist = Math.sqrt(p.width*p.width + p.height*p.height);
+
+    // p.imgScale = p.height * 0.0006;   
+    p.imgScale = p.euclidDist * 0.00025;   
 
   }
 
@@ -55,12 +59,12 @@ var sketch = function(p) {
     p.noErase();
 
     // draw head shot
-    p.drawFeature(p.headshot, 0, 0, 0, 1.4, false);
+    p.drawFeature(p.headshot, 0, 0, 0, 1.4, 0.3);
 
     // add extra eyes
-    p.drawFeature(p.eye1, 240, 50, 1, 1.01, true);
-    p.drawFeature(p.eye2, -270, -20, 2, 1.01, true);
-    p.drawFeature(p.eye3, 100, -230, 3, 1.01, true);
+    p.drawFeature(p.eye1, 450, 200, 1, 1.01, 1);
+    p.drawFeature(p.eye2, -550, -200, 2, 1.01, 1);
+    // p.drawFeature(p.eye3, 200, -500, 3, 1.01, 1);
 
     // randomly change some features
     p.changeFeature();
@@ -76,11 +80,16 @@ var sketch = function(p) {
   }
 
   p.getCenterPos = function() {
+
     p.centreX = p.width*0.7;
     p.centreY = p.height/2;
+
+    if (width < 700) {
+      p.centreX = p.width/2;
+    }
   }
 
-  p.drawFeature = function(feature, distX, distY, num, scale, animate) {
+  p.drawFeature = function(feature, distX, distY, num, scale, animateScale) {
     // draws the given facial feature (img) to the canvas, 
     // relative to center point (2/3*width 1/2*height)
 
@@ -89,24 +98,19 @@ var sketch = function(p) {
       p.count = p.frameCount - 1;
     }
 
-    p.randOffsetX = 0;
-    p.randOffsetY = 0;
-
-    if (animate) {
+    // p.randOffsetX = 0;
+    // p.randOffsetY = 0;
   
-      p.randOffsetX = p.noise(0, p.count/p.frameChange, num) * p.randomness;
-      p.randOffsetX = p.randOffsetX-(p.randomness/2);
-      p.randOffsetY = p.noise(p.count/p.frameChange, 0 , num) * p.randomness;
-      p.randOffsetY = p.randOffsetY-(p.randomness/2);
-
-    }
+    p.randOffsetX = p.noise(0, p.count/p.frameChange, num) * p.randomness;
+    p.randOffsetX = (p.randOffsetX-(p.randomness/2)) * animateScale;
+    p.randOffsetY = p.noise(p.count/p.frameChange, 0 , num) * p.randomness;
+    p.randOffsetY = (p.randOffsetY-(p.randomness/2))   * animateScale;
   
     // p.featureX = p.centreX - p.featureWidth/2 + (distX+p.randOffsetX) * p.imgScale;
-    p.featureX = p.centreX + distX + p.randOffsetX;
+    p.featureX = p.centreX + (distX + p.randOffsetX) * p.imgScale;
     // p.featureY = p.height*0.45 - p.featureHeight/2 + (distY+p.randOffsetY) * p.imgScale;
-    p.featureY = p.centreY + distY + p.randOffsetY;
+    p.featureY = p.centreY + (distY + p.randOffsetY) * p.imgScale;
     p.image(feature, p.featureX, p.featureY, feature.width * p.imgScale * scale, feature.height * p.imgScale * scale);
-    console.log('here');
   }
 
   p.changeFeature = function() {
